7795a986cbc480fa2b7619711f7bda8a967c49e6
unknown <stm@stm1.delcam.local>
2015-12-07 16:27:21 +0000

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,13 @@
+/**
+ * Created by stm on 07/12/2015.
+ */
+import {bootstrap, Component} from 'angular2/angular2';
+
+@Component({
+    selector:'my-app',
+    template: '<h1>My Second Angular 2 App</h1>'
+})
+
+class AppComponent {}
+
+bootstrap(AppComponent);
